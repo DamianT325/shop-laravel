03f87c43e701e6cba433fc998eb2a22bc9ea4d01
DamianT325 <dtramowski789@interia.pl>
2023-01-18 09:28:28 +0100

    Create ProductCategorySeeder and display in template

--- app/Http/Controllers/MainPage.php
+++ app/Http/Controllers/MainPage.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 use App\Models\Product_With_Image;
 
@@ -10,7 +11,8 @@ class MainPage extends Controller
 {
     public function index() {
         return view('main', [
-            'products' => Product_With_Image::paginate(6)
+            'products' => Product_With_Image::paginate(6),
+            'categories' => ProductCategory::orderBy('name', 'ASC')->get()
         ]);
     }
 }
--- database/seeders/Users.php
+++ database/seeders/Users.php
@@ -1,22 +1,22 @@
 <?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
-class Users extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        App\Users::create([
-            'name' => 'A New Hope',
-            'year' => '1977'
-        ]);
-    }
-}
+//
+//namespace Database\Seeders;
+//
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Seeder;
+//
+//class Users extends Seeder
+//{
+//    /**
+//     * Run the database seeds.
+//     *
+//     * @return void
+//     */
+//    public function run()
+//    {
+//        App\Users::create([
+//            'name' => 'A New Hope',
+//            'year' => '1977'
+//        ]);
+//    }
+//}
